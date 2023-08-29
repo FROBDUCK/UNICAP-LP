6e88ca4cbb89530486265c836ac726081efc365c
FROBDUCK <100322056+FROBDUCK@users.noreply.github.com>
2023-08-29 19:06:46 -0300

    Create LISTA1.java

---
+++ LISTA1.java
@@ -0,0 +1,61 @@
+import org.junit.experimental.results.PrintableResult;
+
+class Main {
+  public static void main(String[] args) {
+    /* 1)
+    int a = 5;
+
+    if(a % 2==0){
+      System.out.println("par");
+    }else{
+      System.out.println("impar");
+    }
+    */
+    
+    /*2)
+    int a = 9;
+    int b = 5;
+    int c = 13;
+
+    if (a+b>c){
+      System.out.println("maior");
+      }else {
+      System.out.println("menor");
+      }
+    */
+    
+    /* 3)
+    int a = 6;
+    int b = 5;
+    int c;
+    
+
+    if(a==b){
+      c = a + b;
+        System.out.println(c);
+    }else{
+      c = a * b;
+      System.out.println(c);
+    }*/
+    
+    /* 3)
+    int a = -3;
+
+    if(a>0){
+    System.out.println(a*2);
+    }else{
+      System.out.println(a*3);
+    }
+    */
+    
+    /* 5)
+    int a = 5;
+
+    if(a % 2==0){
+      System.out.println(a + 5);
+    }else{
+      System.out.println(a + 8);
+    }
+    */
+    }
+  }
